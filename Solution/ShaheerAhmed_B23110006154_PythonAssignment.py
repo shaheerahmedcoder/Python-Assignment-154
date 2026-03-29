5d11539b2385695bd0908e28cda10a7e93d7adbc
shaheerahmedcoder <shaheerahmed.0206@gmail.com>
2026-03-29 20:27:51 +0500

    q2 done

--- Solution/ShaheerAhmed_B23110006154_PythonAssignment.py
+++ Solution/ShaheerAhmed_B23110006154_PythonAssignment.py
@@ -17,5 +17,21 @@ for s in range(1500, 2701):
         ans.append(s)
 print(ans)
 
+# --- Q2: Factorial without math.factorial() function --- 
+n = int(input("enter number: "))
+f = 1
+for i in range(1, n+1):
+    f *= i
+print(f)
 
+# --- Q1: Numbers divisible by 7 and multiple of 5, between 1500 and 2700 ---
 
+# --- Q1: Numbers divisible by 7 and multiple of 5, between 1500 and 2700 ---
+
+# --- Q1: Numbers divisible by 7 and multiple of 5, between 1500 and 2700 ---
+
+# --- Q1: Numbers divisible by 7 and multiple of 5, between 1500 and 2700 ---
+
+# --- Q1: Numbers divisible by 7 and multiple of 5, between 1500 and 2700 ---
+
+# --- Q1: Numbers divisible by 7 and multiple of 5, between 1500 and 2700 ---
