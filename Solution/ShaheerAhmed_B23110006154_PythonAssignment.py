76be135b1dfae51902619d1d9e1a9b82701696c0
shaheerahmedcoder <shaheerahmed.0206@gmail.com>
2026-03-29 21:34:38 +0500

    assignment completed

--- Solution/ShaheerAhmed_B23110006154_PythonAssignment.py
+++ Solution/ShaheerAhmed_B23110006154_PythonAssignment.py
@@ -72,4 +72,80 @@ for i in range(1, small+ 1):
         hcf = i 
 print(f"HCF = {hcf}")
 
-# --- Q1: Numbers divisible by 7 and multiple of 5, between 1500 and 2700 ---
+# ------------------------------
+# Section 2: Function Questions(In pdf for loop questions was written and the questions was more towards funtions concept thats why i changed section(2) heading by myself)
+# ------------------------------
+
+# --- Q1: Area of Rectangle --- 
+def ra(length, width):
+    return length * width
+print(ra(2, 3))
+
+# --- Q2: Even and Odd ---
+def ev_od(n):
+    if n % 2 == 0:
+        return "even"
+    return "odd"
+ 
+print(ev_od(2))   
+print(ev_od(5))   
+
+# --- Q3: Reverse a String ---
+def revstr(s):
+    rev = ""
+    for i in s:
+        rev = i + rev
+    return rev
+ 
+print(revstr("shaheer")) 
+
+# --- Q4: Sum of all Digits---
+def sumdig(n):
+    n = abs(n)   
+    t = 0
+    for d in str(n):
+        t += int(d)
+    return t
+ 
+print(sumdig(154))   # 10
+
+# --- Q5: Prime number check---
+def prime(n):
+    if n < 2:
+        return False
+    for i in range(2, n):
+        if n % i == 0:
+            return False
+    return True
+ 
+print(prime(154))    
+print(prime(7))   
+
+# --- Q6:  Fibonacci Series prob. ---
+def fib(n):
+    a, b = 0, 1
+    for i in range(n):
+        print(a, end=" ")
+        a, b = b, a + b
+    
+fib(8)   
+
+# --- Q7:  Counting the vowels prob. ---
+def vow(s):
+    count = 0
+    for i in s.lower():
+        if i in "aeiou":
+            count = count + 1
+    return count
+ 
+print(vow("shaheer ahmed"))   
+
+# --- Q8: Power without pow() or anyy operator ---
+def pow(baseVal, exp):
+    res = 1
+    for i in range(exp):
+        res = res * baseVal
+    return res
+ 
+print(pow(5, 2))   
+
