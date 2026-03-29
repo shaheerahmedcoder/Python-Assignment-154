b958ac73591df7af95ffddf14357099084e72f7f
shaheerahmedcoder <shaheerahmed.0206@gmail.com>
2026-03-29 14:35:24 +0500

    info added

---
+++ Solution/ShaheerAhmed_B23110006154_PythonAssignment.py.py
@@ -0,0 +1,7 @@
+# ==============================================
+# Name          : Shaheer Ahmed
+# Roll No       : B23110006154
+# Class         : BSCS - Section A
+# Assignment    : Python Practice Assignment
+# Semester/Year : 5th / 3rd
+# ==============================================
